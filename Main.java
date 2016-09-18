4ff3b46c38572b34af0e152348101ee34c3ccc04
kameshsuri <ameshsuri419@gmail.com>
2016-09-18 20:08:54 +0530

    it is for creating local branch

--- Main.java
+++ Main.java
@@ -5,6 +5,6 @@ public static void main(String[] args)
 System.out.println("haiii git");
 System.out.println("modification changes by local repo");
 System.out.println("added lines in remote repositor helllooo hyd");
-
+System.out.println("it is for branch");
 }
 }
