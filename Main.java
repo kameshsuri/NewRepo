ac7f882caedf448afce8bb63c6f292cfaf1db48e
kameshsuri <ameshsuri419@gmail.com>
2016-09-18 20:17:24 +0530

    remote to local

--- Main.java
+++ Main.java
@@ -6,5 +6,7 @@ System.out.println("haiii git");
 System.out.println("modification changes by local repo");
 System.out.println("added lines in remote repositor helllooo hyd");
 System.out.println("it is for branch");
+System.out.println("it is pulling remote to local");
+
 }
 }
