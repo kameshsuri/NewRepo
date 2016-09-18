a1656930ebdd80dfc444d335965a018acc2515ed
kameshsuri <kameshsuri419@gmail.com>
2016-09-18 20:25:39 +0530

    eclipse using

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ System.out.println("haiii git");
 System.out.println("modification changes by local repo");
 System.out.println("added lines in remote repositor helllooo hyd");
 System.out.println("it is for branch");
+System.out.println("it is from eclipse");
 }
 }
